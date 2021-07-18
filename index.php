516f1f9911502c94a7f4ef58e31a63ccb6ac1bf7
Russkazkin <ruslan@skazkin.su>
2021-07-18 08:21:20 +0300

    TASK-8: Resolve TASK-8.1

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 
+use TASK8_1\Explorer;
+
 spl_autoload_register(function ($className) {
     $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
     require_once __DIR__ . DIRECTORY_SEPARATOR . $className . '.php';
 });
+
+echo 'Задание 1' . PHP_EOL;
+$explorer = new Explorer();
+$explorer->printDirContent('misc');
+echo "_______________________" . PHP_EOL;
+$explorer->printDirContent('FLA');
+echo "_______________________" . PHP_EOL;
+$explorer->printDirContent('..');
+echo "_______________________" . PHP_EOL;
+$explorer->printDirContent('FLB');
